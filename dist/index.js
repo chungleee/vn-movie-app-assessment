85fa4c41eb331f421ee3b48d5624e0114a503cf8
Leon <leon.karho.chung@gmail.com>
2023-06-03 17:02:37 -0400

    compiled to js files

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { PrismaClient } from "@prisma/client";
 import { typeDefs } from "./schema/index.js";
 import { resolvers } from "./resolvers/index.js";
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
+// prisma client setup and export
+export const prisma = new PrismaClient();
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 });
--- dist/schema/index.js
+++ dist/schema/index.js
@@ -1,5 +1,33 @@
 export const typeDefs = `#graphql
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    movies: [Movie]
+  }
+  
+   type Movie {
+    id: String!
+    movieName: String!
+    description: String!
+    directorName: String!
+    releaseDate: String
+    movieOwner: User
+  }
+
+  type RegisteredOrLoginUser {
+    id: String!
+    username:String!
+    email:String!
+    token: String
+  }
+
   type Query {
     _empty:String
+  }
+
+  type Mutation {
+    register(username: String, email: String, password: String): RegisteredOrLoginUser!
   }
 `;
