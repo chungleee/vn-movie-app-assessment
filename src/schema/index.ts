8524cdbf15b750a6c7f05f5fd7025ca9c69aa8c5
Leon <leon.karho.chung@gmail.com>
2023-06-03 17:01:53 -0400

    movie type, register user input type, register mutation

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -6,7 +6,28 @@ export const typeDefs = `#graphql
     password: String!
     movies: [Movie]
   }
+  
+   type Movie {
+    id: String!
+    movieName: String!
+    description: String!
+    directorName: String!
+    releaseDate: String
+    movieOwner: User
+  }
+
+  type RegisteredOrLoginUser {
+    id: String!
+    username:String!
+    email:String!
+    token: String
+  }
+
   type Query {
     _empty:String
+  }
+
+  type Mutation {
+    register(username: String, email: String, password: String): RegisteredOrLoginUser!
   }
 `;
