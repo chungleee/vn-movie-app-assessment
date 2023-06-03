d8173af977b124909a43ae7997176190916fbb0b
Leon <leon.karho.chung@gmail.com>
2023-06-03 16:51:47 -0400

    user type and database url please dont steal

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,4 +1,11 @@
 export const typeDefs = `#graphql
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    movies: [Movie]
+  }
   type Query {
     _empty:String
   }
