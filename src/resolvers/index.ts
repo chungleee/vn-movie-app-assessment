7958ed4ffbb03ccaa910db1497e99b76454612f4
Leon <leon.karho.chung@gmail.com>
2023-06-03 17:07:19 -0400

    login mutation function

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,5 +1,7 @@
 import argon2 from "argon2";
 import { prisma } from "../index.js";
+import { GraphQLError } from "graphql";
+import jwt from "jsonwebtoken";
 
 const UserMutations = {
 	register: async (parent, { username, email, password }, contextValue) => {
@@ -15,6 +17,32 @@ const UserMutations = {
 		delete user.password;
 
 		return user;
+	},
+	login: async (parent, { email, password }, contextValue) => {
+		try {
+			const user = await prisma.user.findUnique({
+				where: {
+					email,
+				},
+			});
+
+			if (await argon2.verify(user.password, password)) {
+				const token = await jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+					expiresIn: "1h",
+				});
+				console.log(token);
+				return { ...user, token };
+			} else {
+				throw new GraphQLError("Email or password is incorrect", {
+					extensions: {
+						code: "BAD_REQUEST",
+					},
+				});
+			}
+		} catch (error) {
+			console.log("error: ", error);
+			return error;
+		}
 	},
 };
 
