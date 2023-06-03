3860073b82f3bd60eef047ab0b6c6846b7902195
Leon <leon.karho.chung@gmail.com>
2023-06-03 17:10:00 -0400

    check if token is in header, if true - pass user to context - if false, pass empty obj

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,9 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { PrismaClient } from "@prisma/client";
 import { typeDefs } from "./schema/index.js";
 import { resolvers } from "./resolvers/index.js";
+import jwt from "jsonwebtoken";
+import { GraphQLError } from "graphql";
+import "dotenv/config.js";
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -11,5 +14,27 @@ const server = new ApolloServer({
 export const prisma = new PrismaClient();
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async ({ req, res }) => {
+        try {
+            const token = req.headers.authorization || "";
+            let user = {};
+            if (!token) {
+                return {};
+            }
+            else {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                user = await prisma.user.findUnique({
+                    where: {
+                        id: decoded.id,
+                    },
+                    select: { id: true, email: true },
+                });
+            }
+            return { user };
+        }
+        catch (error) {
+            throw new GraphQLError(error);
+        }
+    },
 });
 console.log(`Server ready at ${url}`);
