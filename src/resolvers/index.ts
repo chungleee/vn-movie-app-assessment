abdc93a3e9fefdc64f9b8f8e364a6c30ca0a7e82
Leon <leon.karho.chung@gmail.com>
2023-06-03 17:02:21 -0400

    register mutation function

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,30 @@
+import argon2 from "argon2";
+import { prisma } from "../index.js";
+
+const UserMutations = {
+	register: async (parent, { username, email, password }, contextValue) => {
+		const hash = await argon2.hash(password);
+		const user = await prisma.user.create({
+			data: {
+				username,
+				email,
+				password: hash,
+			},
+		});
+
+		delete user.password;
+
+		return user;
+	},
+};
+
 export const resolvers = {
 	Query: {
 		_empty: () => {
 			return "";
 		},
+	},
+	Mutation: {
+		...UserMutations,
 	},
 };
